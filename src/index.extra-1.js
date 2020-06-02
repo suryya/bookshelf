fdf051ab43adbae067b09f70d4f067c2554de2e7
Kent C. Dodds <me+github@kentcdodds.com>
2020-06-02 10:22:58 -0600

    exercises/07-context

---
+++ src/index.extra-1.js
@@ -0,0 +1,28 @@
+import {loadDevTools} from './dev-tools/load'
+import './bootstrap'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {ReactQueryConfigProvider} from 'react-query'
+import {AuthProvider} from './context/auth-context'
+import {App} from './app'
+
+const queryConfig = {
+  retry(failureCount, error) {
+    if (error.status === 404) return false
+    else if (failureCount < 2) return true
+    else return false
+  },
+  useErrorBoundary: true,
+  refetchAllOnWindowFocus: false,
+}
+
+loadDevTools(() => {
+  ReactDOM.render(
+    <ReactQueryConfigProvider config={queryConfig}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ReactQueryConfigProvider>,
+    document.getElementById('root'),
+  )
+})
